249ceedc2c1cc00b34804f8e7d8873b7d9959c8d
SoSaymon <szymon.chirowski@protonmail.com>
2023-08-04 00:46:26 +0200

    fix(tests): fix parameters for test_get_and_verify_color

--- tests/test_utils/test_get_and_verify_space_options.py
+++ tests/test_utils/test_get_and_verify_space_options.py
@@ -1,13 +1,13 @@
-import pytest
 import sys
 
+import pytest
 from pytest_mock import MockFixture
 
 sys.path.append("..")
 
 from src.utils.get_and_verify_space_options import get_and_verify_space_id, \
     get_and_verify_space_name, get_and_verify_description, get_and_verify_initials, get_and_verify_color, \
-    get_and_verify_disabled_features, get_and_verify_image_url
+    get_and_verify_disabled_features
 
 
 @pytest.mark.space_options
@@ -131,18 +131,19 @@ class TestSpaceOptions:
             ('#000000 ', '#000000'),
             (' #000000', '#000000'),
             (' #000000 ', '#000000'),
-            ('#000', '#000'),
-            ('#000 ', '#000'),
-            (' #000', '#000'),
-            (' #000 ', '#000'),
-            ('000', '#000'),
-            ('000 ', '#000'),
-            (' 000', '#000'),
-            (' 000 ', '#000'),
+            ('#000', '#000000'),
+            ('#000 ', '#000000'),
+            (' #000', '#000000'),
+            (' #000 ', '#000000'),
+            ('000', '#000000'),
+            ('000 ', '#000000'),
+            (' 000', '#000000'),
+            (' 000 ', '#000000'),
             ('000000', '#000000'),
             ('000000 ', '#000000'),
             (' 000000', '#000000'),
             (' 000000 ', '#000000'),
+            ('#FAF', '#FFAAFF'),
             ],
     )
     def test_get_and_verify_color(self, mocker: MockFixture, color: str, expected: str):
@@ -187,4 +188,3 @@ class TestSpaceOptions:
     def test_get_and_verify_disabled_features(self, mocker: MockFixture, disabled_features: str, expected: str):
         mocker.patch("builtins.input", return_value=disabled_features)
         assert get_and_verify_disabled_features() == expected
-
