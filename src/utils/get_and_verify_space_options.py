3493f87e54f2788b8d7b5f0297497fccc25efe0c
SoSaymon <szymon.chirowski@protonmail.com>
2023-08-04 17:50:24 +0200

    feat(space_options): refactor get_and_verify_space_options

--- src/utils/get_and_verify_space_options.py
+++ src/utils/get_and_verify_space_options.py
@@ -1,13 +1,21 @@
 import re
-from typing import List, Tuple
+from typing import List, Tuple, Optional, Callable
+
+from src.utils.space_options_utils.verifiers import verify, verify_space_id, verify_initials_len, \
+    verify_if_color_starts_with_hash, verify_if_color_has_seven_characters, verify_color_regex_pattern, \
+    verify_if_string_starts_with_comma, verify_if_space_exists_near_comma, verify_if_string_ends_with_comma, \
+    verify_disabled_features_regex_pattern, verify_if_image_url_is_base64_encoded, \
+    verify_if_image_url_has_image_type, verify_if_image_url_starts_with_data_image
+
+from src.utils.space_options_utils.utils import get_image_url_image_type, get_input
 
 SPACE_ID = str
 SPACE_NAME = str
-DESCRIPTION = str | None
-INITIALS = str | None
-COLOR = str | None
-DISABLED_FEATURES = List[str] | None
-IMAGE_URL = str | None
+DESCRIPTION = Optional[str]
+INITIALS = Optional[str]
+COLOR = Optional[str]
+DISABLED_FEATURES = Optional[List[str]]
+IMAGE_URL = Optional[str]
 
 SpaceData = Tuple[SPACE_ID, SPACE_NAME, DESCRIPTION, INITIALS, COLOR, DISABLED_FEATURES, IMAGE_URL]
 
@@ -28,159 +36,154 @@ def get_and_verify_space_options() -> SpaceData:
     return space_id, space_name, description, initials, color, disabled_features, image_url
 
 
-def get_and_verify_space_id() -> SPACE_ID:
+def get_and_verify_space_id(get_space_id_fn: Callable[[str], str] = get_input) -> SPACE_ID:
     """
     Get and verify the space ID
     :return:  String space ID
     """
     while True:
-        space_id = input('Enter the space ID (Required): ')
+        space_id = get_space_id_fn('Enter the space ID (Required): ')
+        space_id = space_id.strip().lower()
 
-        if space_id:
-            space_id = space_id.strip().lower()
-
-            pattern = r'^[a-z0-9_-]+$'
-            if re.match(pattern, space_id):
-                return space_id
+        if verify_space_id(space_id):
+            return space_id
         print('Space ID is required.')
 
 
-def get_and_verify_space_name() -> SPACE_NAME:
+def get_and_verify_space_name(get_space_name_fn: Callable[[str], str] = get_input) -> SPACE_NAME:
     """
     Get and verify the space name
     :return:  String space name
     """
     while True:
-        space_name = input('Enter the space name (Required): ')
+        space_name = get_space_name_fn('Enter the space name (Required): ')
+        space_name = space_name.strip()
 
-        if space_name:
-            space_name = space_name.strip()
+        if verify(space_name):
             return space_name
         print('Space name is required.')
 
 
-def get_and_verify_description() -> DESCRIPTION:
+def get_and_verify_description(get_space_description: Callable[[str], str] = get_input) -> DESCRIPTION:
     """
     Get and verify the description
     :return:  String description or None
     """
-    description = input('Enter the space description (Optional): ')
+    description = get_space_description('Enter the space description (Optional): ')
+    description = description.strip()
 
-    if description:
-        description = description.strip()
+    if verify(description):
         return description
     return None
 
 
-def get_and_verify_initials() -> INITIALS:
+def get_and_verify_initials(get_space_initials_fn: Callable[[str], str] = get_input) -> INITIALS:
     """
     Get and verify the initials
     :return:  String initials or None
     """
     while True:
-        initials = input('Enter the space initials (Optional, max length 2): ')
+        initials = get_space_initials_fn('Enter the space initials (Optional, max length 2): ')
+        initials = initials.strip()
 
-        if initials:
-            initials = initials.strip()
+        if verify(initials):
 
-            if 1 <= len(initials) <= 2 and initials.isalpha():
+            if verify_initials_len(initials):
                 return initials.upper()
-            elif not 1 <= len(initials) <= 2:
+            else:
                 print('Initials must be between 1 and 2 characters. Example: CJ or C')
                 print('If you do not want to set initials, leave the field blank.')
                 continue
         return None
 
 
-def get_and_verify_color() -> COLOR:
+def get_and_verify_color(get_space_color_fn: Callable[[str], str] = get_input) -> COLOR:
     """
     Get and verify the color
     :return:  String hex color or None
     """
     while True:
-        color = input('Enter the space color in hex format (Optional): ')
+        color = get_space_color_fn('Enter the space color (Optional, hex format): ')
+        color = color.strip()
 
-        if color:
-            color = color.strip()
+        if verify(color):
 
-            if not color.startswith('#'):
+            if not verify_if_color_starts_with_hash(color):
                 color = f'#{color}'
 
-            if not len(color) == 7:
+            if not verify_if_color_has_seven_characters(color):
                 color = color[0] + color[1] + color[1] + color[2] + color[2] + color[3] + color[3]
 
-            pattern = r'^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$'
-            if re.match(pattern, color):
+            if verify_color_regex_pattern(color):
                 return color
-            elif not re.match(pattern, color) and color != '':
+            else:
                 print('Color must be in hex format. Example: #000000')
                 print('If you do not want to set a color, leave the field blank.')
                 continue
         return None
 
 
-def get_and_verify_disabled_features() -> DISABLED_FEATURES:
+def get_and_verify_disabled_features(
+        get_space_disabled_features_fn: Callable[[str], str] = get_input) -> DISABLED_FEATURES:
     """
     Get and verify the disabled features
     :return:  List of disabled features or None
     """
     while True:
-        disabled_features = input('Enter the space disabled features (Optional, comma separated): ')
+        disabled_features = get_space_disabled_features_fn(
+            'Enter the space disabled features (Optional, comma separated): ')
+        disabled_features = disabled_features.strip()
 
-        if disabled_features:
-            disabled_features = disabled_features.strip()
+        if verify(disabled_features):
 
-            if re.search(r',\s', disabled_features):
+            if verify_if_space_exists_near_comma(disabled_features):
                 disabled_features = disabled_features.replace(', ', ',')
-
-            if re.search(r'\s,', disabled_features):
                 disabled_features = disabled_features.replace(' ,', ',')
 
-            if disabled_features.startswith(','):
+            if verify_if_string_starts_with_comma(disabled_features):
                 disabled_features = disabled_features[1:]
 
-            if disabled_features.endswith(','):
+            if verify_if_string_ends_with_comma(disabled_features):
                 disabled_features = disabled_features[:-1]
 
-            pattern = r'^[a-zA-Z0-9_-]+(,[a-zA-Z0-9_-]+)*$'
-            if re.match(pattern, disabled_features):
+            if verify_disabled_features_regex_pattern(disabled_features):
                 return disabled_features.split(',')
-            elif not re.match(pattern, disabled_features) and disabled_features != '':
+            else:
                 print('Disabled features must be comma separated. Example: feature1,feature2')
                 print('If you do not want to set disabled features, leave the field blank.')
                 continue
         return None
 
 
-def get_and_verify_image_url() -> IMAGE_URL:
+def get_and_verify_image_url(get_space_image_url_fn: Callable[[str], str] = get_input) -> IMAGE_URL:
     """
     Get and verify the image URL
     :return:  String image URL or None
     """
+    valid_image_types: List[str] = ["png", "jpeg", "gif", "webp"]
+
     while True:
-        image_url = input('Enter the space image URL (Optional): ')
+        image_url = get_space_image_url_fn('Enter the space image URL (Optional): ')
+        image_url = image_url.strip()
 
-        if image_url:
-            image_url = image_url.strip()
+        if verify(image_url):
 
-            if not image_url.startswith("data:image/"):
+            if not verify_if_image_url_starts_with_data_image(image_url):
                 print("Image URL must start with 'data:image'")
                 print('If you do not want to set an image URL, leave the field blank.')
                 continue
 
-            image_type_match = re.match(r"data:image/(\w+);", image_url)
-            if not image_type_match:
-                print("Image URL must have a valid image type")
+            if not verify_if_image_url_has_image_type(image_url):
+                print("Image URL must have a image type")
                 print('If you do not want to set an image URL, leave the field blank.')
                 continue
 
-            image_type = image_type_match.group(1)
-            if image_type not in ["png", "jpeg", "gif", "webp"]:
-                print("Image URL must have a valid image type")
+            if get_image_url_image_type(image_url) not in valid_image_types:
+                print("Image URL must have a valid image type (png, jpeg, gif, webp)")
                 print('If you do not want to set an image URL, leave the field blank.')
                 continue
 
-            if not re.search(r"base64,", image_url):
+            if not verify_if_image_url_is_base64_encoded(image_url):
                 print("Image URL must be base64 encoded")
                 print('If you do not want to set an image URL, leave the field blank.')
                 continue
---
+++ src/utils/space_options_utils/verifiers.py
@@ -0,0 +1,159 @@
+import re
+from typing import List, Optional
+
+SPACE_ID = str
+SPACE_NAME = str
+DESCRIPTION = Optional[str]
+INITIALS = Optional[str]
+COLOR = Optional[str]
+DISABLED_FEATURES = Optional[List[str]]
+IMAGE_URL = Optional[str]
+
+
+def verify(value: Optional[str]) -> bool:
+    """
+    Verify the value
+    :param value:  String value
+    :return:  Boolean True if valid, False if invalid
+    """
+    if value:
+        return True
+    return False
+
+
+def verify_space_id(space_id: SPACE_ID) -> bool:
+    """
+    Verify the space ID
+    :param space_id:  String space ID
+    :return:  Boolean True if valid, False if invalid
+    """
+    if space_id:
+        pattern = r'^[a-z0-9_-]+$'
+        if re.match(pattern, space_id):
+            return True
+    return False
+
+
+def verify_initials_len(initials: INITIALS) -> bool:
+    """
+    Verify if the initials are between 1 and 2 characters
+    :param initials:  String initials
+    :return:  Boolean True if valid, False if invalid
+    """
+    if initials:
+        if 1 <= len(initials) <= 2:
+            return True
+    return False
+
+
+def verify_if_color_starts_with_hash(color: COLOR) -> bool:
+    """
+    Verify if the color starts with a hash
+    :param color:  String color
+    :return:  Boolean True if valid, False if invalid
+    """
+    if color.startswith('#'):
+        return True
+    return False
+
+
+def verify_if_color_has_seven_characters(color: COLOR) -> bool:
+    """
+    Verify if the color has seven characters
+    :param color:  String color
+    :return:  Boolean True if valid, False if invalid
+    """
+    if len(color) == 7:
+        return True
+    return False
+
+
+def verify_color_regex_pattern(color: COLOR) -> bool:
+    """
+    Verify the color regex pattern
+    :param color:  String color
+    :return:  Boolean True if valid, False if invalid
+    """
+    pattern = r'^#([A-Fa-f0-9]{6})$'
+    if re.match(pattern, color):
+        return True
+    return False
+
+
+def verify_if_space_exists_near_comma(value: str) -> bool:
+    """
+    Verify if the space exists after a comma in disabled features
+    :param value:  String value
+    :return:  Boolean True if valid, False if invalid
+    """
+    if re.search(r',\s', value) or re.search(r'\s,', value):
+        return True
+    return False
+
+
+def verify_if_string_starts_with_comma(value: str) -> bool:
+    """
+    Verify if the string starts with a comma
+    :param value:  String value
+    :return:  Boolean True if valid, False if invalid
+    """
+    if value.startswith(','):
+        return True
+    return False
+
+
+def verify_if_string_ends_with_comma(value: str) -> bool:
+    """
+    Verify if the string ends with a comma
+    :param value:  String value
+    :return:  Boolean True if valid, False if invalid
+    """
+    if value.endswith(','):
+        return True
+    return False
+
+
+def verify_disabled_features_regex_pattern(disabled_features: str) -> bool:
+    """
+    Verify the disabled features regex pattern
+    :param disabled_features:  String disabled features
+    :return:  Boolean True if valid, False if invalid
+    """
+    pattern = r'^[a-zA-Z0-9_-]+(,[a-zA-Z0-9_-]+)*$'
+    if re.match(pattern, disabled_features):
+        return True
+    return False
+
+
+def verify_if_image_url_starts_with_data_image(image_url: IMAGE_URL) -> bool:
+    """
+    Verify if the image URL starts with data:image
+    :param image_url:  String image URL
+    :return:  Boolean True if valid, False if invalid
+    """
+    if image_url.startswith('data:image'):
+        return True
+    return False
+
+
+def verify_if_image_url_has_image_type(image_url: IMAGE_URL) -> bool:
+    """
+    Verify if the image URL has an image type
+    :param image_url:  String image URL
+    :return:  Boolean True if valid, False if invalid
+    """
+    image_type_match = re.match(r"data:image/(\w+);", image_url)
+    if image_type_match:
+        return True
+    return False
+
+
+def verify_if_image_url_is_base64_encoded(image_url: IMAGE_URL) -> bool:
+    """
+    Verify if the image URL is base64 encoded
+    :param image_url:  String image URL
+    :return:  Boolean True if valid, False if invalid
+    """
+    if re.search(r"base64,", image_url):
+        return True
+    return False
